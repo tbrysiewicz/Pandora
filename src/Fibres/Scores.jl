b6bf7fee84c9aaeef8af97b11ee4e735155c7eb9
tbrysiewicz <taylorbrysiewicz@gmail.com>
2024-05-24 09:50:54 -0400

    explaining git example

--- src/Fibres/Scores.jl
+++ src/Fibres/Scores.jl
@@ -6,3 +6,4 @@ struct Score
     ScoreFunction #must accept Tuple{Result, Vector{ComplexF64}} and return anything in T
     gt #must accept two elements of T and return true/false true = gt or eq
 end
+ 
