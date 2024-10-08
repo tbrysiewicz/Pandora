5668ed93ca8c1bfb2d434b4622ac9ef42120a088
dave-k-johnson <davidklassenjohnson@gmail.com>
2024-10-08 08:54:40 -0400

    Update AltBurmester.jl
    added AltsProblem function

--- src/Examples/AltBurmester.jl
+++ src/Examples/AltBurmester.jl
@@ -1,6 +1,6 @@
 export
-    AltBurmester
-
+    AltBurmester,
+    AltsProblem
 
 
 
@@ -131,4 +131,102 @@ function AltBurmester(M,N)
     return(E)
 end
 
+function AltsProblem()
+    
+
+
+    @var G1r,G2r,G1i,G2i,z1r,z2r,z1i,z2i,dx[1:9],dy[1:9],tr[1:9],ti[1:9]
+
+    #initializing the location vectors and orientation angle vectors, lowercase and uppercase pairs indicating 
+    #pairs of isotropic coordinates. tr and ti are the real and imaginary parts of the orientation angles
+
+    d=[dx[1]-dx[1]+(dy[1]-dy[1])*im] #location of coupler point - represented in the dx and dy variables.
+    D=[dx[1]-dx[1]-(dy[1]-dy[1])*im] #conjugate location vector
+    
+
+	#Construct the coupler point locations as functions of dx and dy
+    for i in 1:8
+        push!(d,(dx[i+1]-dx[1])+(dy[i+1]-dy[1])*im);
+        push!(D,(dx[i+1]-dx[1])-(dy[i+1]-dy[1])*im);
+    end
+
+
+    #g1,g2 are the pinned point vectors at first interpolation
+    g1=G1r+im*G1i;
+    g2=G2r+im*G2i;
+    z1=z1r+im*z1i; #z1/z2 are circle point vectors at first interpolation (others are functions of these and the theta angles)
+    z2=z2r+im*z2i;
+
+
+    G1=G1r-im*G1i;
+    G2=G2r-im*G2i;
+    Z1=z1r-im*z1i;
+    Z2=z2r-im*z2i;
+    
+	
+    #setting equation (2), (5), (6), (7) (8), (9) (10) in 
+    #the paper
+
+
+    eq2 = []
+    for i in 2:9
+	   push!(eq2,(tr[i]+im*ti[i])*(tr[i]-im*ti[i])-1)
+    end
+    
+
+    eq5=[z1-g1];
+    eq6=[Z1-G1];
+    eq8=[z2-g2];
+    eq9=[Z2-G2];
+
+
+    for i in 2:9
+        push!(eq5,(tr[i]+im*ti[i])*z1+d[i]-g1)
+        push!(eq6,(tr[i]-im*ti[i])*Z1+D[i]-G1)
+        push!(eq8,(tr[i]+im*ti[i])*z2+d[i]-g2)
+        push!(eq9,(tr[i]-im*ti[i])*Z2+D[i]-G2)
+    end
+
+
+    eq7=[]
+    eq10=[]
+    for i in 2:9
+        push!(eq7,eq5[i]*eq6[i]-eq5[1]*eq6[1])
+        push!(eq10,eq8[i]*eq9[i]-eq8[1]*eq9[1])
+    end
+   
+
+    polys = []
+    for i in 1:8
+        push!(polys, eq2[i])
+    end
+
+
+    for i in 1:8
+        push!(polys,eq7[i])
+        push!(polys,eq10[i])
+    end
+    
+    params = Vector{Variable}([])
+
+
+    for i in 1:9
+		push!(params,dx[i])
+        push!(params,dy[i])
+	end
+    
+    push!(params,tr[1])
+    push!(params,ti[1])
+    
+    vars=Vector{Variable}([G1r,G1i,G2r,G2i,z1r,z2r,z1i,z2i])
+
+
+    for i in 2:9
+        push!(vars,tr[i])
+        push!(vars,ti[i])
+    end
+
+    F=System(polys,variables=vars,parameters=params)
+    E=EnumerativeProblem(F)    
+end
 
