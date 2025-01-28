3673f0312574639a7f44c2f7089810fde19e4bcc
tbrysiewicz <taylorbrysiewicz@gmail.com>
2025-01-28 09:58:56 -0500

    forgot one

--- src/FibreVisualization/GraphMesh.jl
+++ src/FibreVisualization/GraphMesh.jl
@@ -1,3 +1,6 @@
+#cache's parameter->f(parameter) input/output pairs for some fibre function
+#  The vector function_cache should NEVER be reordered, as Subdivision refers
+#  to its elements via their indices
 mutable struct GraphMesh
     function_cache :: Vector{Tuple{Vector{Float64},Float64}}
 end
