a5909a75ac98588afe752627109da7253032578a
tbrysiewicz <taylorbrysiewicz@gmail.com>
2025-07-03 19:59:50 -0400

    fixed a latex style issue

--- src/Summarization/background_summary.jl
+++ src/Summarization/background_summary.jl
@@ -43,7 +43,6 @@ function background_summary(EP::EnumerativeProblem;kwargs...)
 
     return(summary)
 end
-
 function tikz_incidence(EP::EnumerativeProblem)
     n = ambient_dimension(EP)
     k = n_polynomials(EP)
@@ -53,19 +52,42 @@ function tikz_incidence(EP::EnumerativeProblem)
     kk = raw"{"*string(k)*raw"}"
     NN = raw"{"*string(N)*raw"}"
 
-s = raw"""
+    s = raw"""
     \begin{center}
     \begin{tikzpicture}
     \matrix (m) [matrix of math nodes,row sep=3em,column sep=0em,minimum width=2em]
     {
-        \mathcal V(F)=\mathcal V("""*raw"""f_1,\ldots,f_"""*kk*raw""")=\{(\textbf{x},\textbf{p}) \mid F(\textbf{x};\textbf{p})=\textbf{0}\} & \subset \mathbb{C}_{\textbf{x}}^"""*nn*raw""" \times \mathbb{C}_{\textbf{p}}^"""*NN*raw""" \\
-        \mathbb{C}_{\p}^"""*NN*raw""" &  \\};
+        \mathcal V(F)=\mathcal V("""
+    if k == 1
+        s *= raw"f_1"
+    elseif k == 2
+        s *= raw"f_1,f_2"
+    elseif k == 3
+        s *= raw"f_1,f_2,f_3"
+    else
+        s *= raw"f_1,\ldots,f_" * kk * raw""
+    end
+    s *= raw""")=\{(\textbf{x},\textbf{p}) \mid F(\textbf{x};\textbf{p})=\textbf{0}\} & \subset \mathbb{C}_{\textbf{x}}^""" * nn * raw""" \times \mathbb{C}_{\textbf{p}}^""" * NN * raw""" \\
+        \mathbb{C}_{\p}^""" * NN * raw""" &  \\};
     \path[-stealth]
         (m-1-1) edge node [left] {$\pi$} (m-2-1);
     \end{tikzpicture}
     \end{center}
     """
-    return(s)
+    return s
+end
+
+function monomial_string(BasePowerPairs)
+    ms = raw""
+    for (v,p) in BasePowerPairs
+        if p != 0
+            ms *= string(v)*raw"^{ "*string(p)*raw"}"
+            println(v)
+            println(p)
+            println(ms)
+        end
+    end
+    return(ms)
 end
 
 function terms_as_text_list(f::Expression,V::Vector{Variable},P::Vector{Variable})
@@ -74,8 +96,7 @@ function terms_as_text_list(f::Expression,V::Vector{Variable},P::Vector{Variable
     M = SC[1][1]
     C = SC[2][1]
     (n,n_terms)=size(M)
-
-    myterms = reverse([raw"{\textbf{"*string(C[i])*raw"}}"*raw" \cdot "*string(prod([V[j]^M[j,i] for j in 1:n])) for i in 1:n_terms])
+    myterms = reverse([raw"{\textbf{"*string(C[i])*raw"}}"*raw" \cdot "*monomial_string([(V[j],M[j,i]) for j in 1:n]) for i in 1:n_terms])
     return(myterms)
 end
 
@@ -87,8 +108,13 @@ function latex_polynomials(EP::EnumerativeProblem;kwargs...)
     for i in 1:length(F)
         f = F[i]
         t = terms_as_text_list(f,V,P)
+        println(t)
         if length(t)<10
-            s *= replace(string(f),"*"=>"")
+            polystring = t[1]
+            for j in 2:length(t)
+                polystring *= raw" + "*t[j]
+            end
+            s *= replace(polystring,"*"=>"")
         else
             f_start = t[1]
             f_start *= "+"*t[2]*"+"*t[3]*"+"
